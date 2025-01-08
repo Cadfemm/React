2826abe5815028091a8aa70a30cf28a06cfa32b3
Cadfemm <154951107+Cadfemm@users.noreply.github.com>
2025-01-08 14:57:30 +0530

    Update stroke.js

--- src/pages/stroke.js
+++ src/pages/stroke.js
@@ -1020,7 +1020,7 @@ const DualPredictionForm = () => {
       boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
       backgroundColor: "#f9f9f9",
     }}>
-      <h3>Improvement Results After Treatment</h3>
+      <h3>Improved MMT Values After Treatment</h3>
       <div className="Results">
         {[
           "Right Lower Limb Hip Flexion",
