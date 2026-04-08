1567a410635769b631b3ae9db112326961345690
raceredge <aman.srivastav@cadfem.ai>
2026-04-08 13:35:03 +0530

    Api interceptors for refresh token, set token to every request, stop refreshing token and etc.

--- src/shared/api/apiClient.js
+++ src/shared/api/apiClient.js
@@ -1,30 +1,66 @@
 import axios from "axios";
+import { BASE_API, API_URL } from "../../platform/config/api.config";
 
+// Store the access token in memory for security reasons (not in localStorage/sessionStorage)
+let accessToken = null;   
+let refreshInterval = null;  // Set refresh interval for access token (e.g., every 5 minutes)
+
+// Create an Axios instance with default settings
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api",
-  headers: {
-    "Content-Type": "application/json"
-  }
+  baseURL: BASE_API,
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" }
 });
+
+// Function to set the access token after login
+export const setAccessToken = (token) => {
+  accessToken = token;
+};
+
+// Function to clear the access token and refresh interval on logout
+export const clearAccessToken = () => {
+  accessToken = null;
+  if (refreshInterval) {
+    clearInterval(refreshInterval);
+  }
+}
+
+// Start automatic refresh of access token every 5 minutes 
+export const startTokenRefresh = () => {
+  refreshInterval = setInterval(() => {
+    try{
+      const res = api.post(
+        BASE_API + API_URL.REFRESH,
+        {},
+        { withCredentials: true }
+      )
+      setAccessToken(res.data.access.access_token);
+    }catch(err){
+      clearInterval(refreshInterval);
+    }
+  }, 4.5 * 60 * 1000)
+};
+
 
 // Attach token automatically (if available)
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    console.log('token interceptor called, accessToken:', accessToken);
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   },
   (error) => Promise.reject(error)
 );
 
+
 // Global response error handling
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      console.warn("Unauthorized – redirect to login");
+      startTokenRefresh();
     }
     return Promise.reject(error);
   }
