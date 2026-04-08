836dde33e950329eebdc6697741887de43d6e14f
raceredge <aman.srivastav@cadfem.ai>
2026-04-08 13:35:19 +0530

    API roots

---
+++ src/platform/config/api.config.js
@@ -0,0 +1,22 @@
+const BASE_API = 'http://localhost:8000/' + 'api/'
+
+const API_URL = {
+    // Users apis
+    USER: BASE_API + 'user/',
+    ME: BASE_API + 'user/me/',
+    LOGIN: BASE_API + 'user/login/',
+    LOGOUT: BASE_API + 'user/logout/',
+
+    // Tokens apis
+    TOKEN: BASE_API + 'token/',
+    VERIFY: BASE_API + 'token/verify/',
+    REFRESH: BASE_API + 'token/refresh/',
+
+    // Forms apis
+    FORM: BASE_API + 'form/',
+}
+
+export {
+    API_URL,
+    BASE_API
+}
