dfb0fba646e0a42a02e8e87dad9103722adde7cf
raceredge <aman.srivastav@cadfem.ai>
2026-04-10 16:16:12 +0530

    Fix token refresh issue

--- src/shared/api/apiClient.js
+++ src/shared/api/apiClient.js
@@ -79,6 +79,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
+      isRefreshing = false
       refreshAccessToken();
     }
     return Promise.reject(error);
